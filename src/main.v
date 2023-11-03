5681e9a5e2b09b5a81e7a6a354430467d06de3f5
werdl <werdl_is_cool@outlook.com>
2023-11-03 07:52:19 +0000

    imprtove syntax highlighting

--- src/main.v
+++ src/main.v
@@ -22,6 +22,11 @@ struct Highlighter {
 	strings RGB
 	brackets1 RGB
 	brackets2 RGB
+	types RGB
+	qualifiers RGB
+	other RGB
+	macros RGB
+	symbols RGB
 }
 
 struct LiveC {
@@ -33,7 +38,7 @@ struct LiveC {
 		last string
 		hl Highlighter
 }
-pub fn (h Highlighter) highlight(input string) []RGB {
+pub fn (h Highlighter) highlight(input string, instring bool) []RGB {
 	mut out:=[]RGB{}
 	conditionals:=[
 		"if",
@@ -45,17 +50,132 @@ pub fn (h Highlighter) highlight(input string) []RGB {
 		"switch",
 		"list"
 	]
+	types:=[
+		"int",
+		"float",
+		"double",
+		"char",
+		"void",
+		"signed",
+		"unsigned",
+		"size_t",
+		'atomic_bool',
+		'atomic_char',
+		'atomic_schar',
+		'atomic_uchar',
+		'atomic_short',
+		'atomic_ushort',
+		'atomic_int',
+		'atomic_uint',
+		'atomic_long',
+		'atomic_ulong',
+		'atomic_llong',
+		'atomic_ullong',
+		'atomic_char16_t',
+		'atomic_char32_t',
+		'atomic_wchar_t',
+		'atomic_size_t',
+		'atomic_ptrdiff_t',
+		'atomic_intmax_t',
+		'atomic_uintmax_t'
+	]
+	qualifiers:=[
+		"const",
+		"volatile",
+		"restrict",
+		"auto",
+		"register",
+		"static",
+		"extern",
+		"thread_local"
+	]
+	other_keywords:=[
+		"struct",
+		"union",
+		"typedef",
+		"enum",
+		"typedef"
+	]
+	macros:=[
+		"#define",
+		"#if",
+		"#endif",
+		"#elseif"
+		"#include"
+		"#ifdef"
+		"#ifndef",
+		"#error",
+		"#pragma"
+	]
+	symbols:=[
+		"&",
+		"|",
+		"!",
+		"%",
+		"^",
+		"*",
+		":",
+		";",
+		"@",
+		"~"
+		"<",
+		">",
+		"?",
+		"/",
+		"=",
+		"-"
+	]
 	mut current:=""
-	mut instr:=false
+	mut instr:=instring
 	for c in input {
 		c_ascii:=c.ascii_str()
 		current+=c_ascii
-		if current in conditionals {
+		if c_ascii in ["'","\"","`"] {
+			instr=!instr
+			out << h.strings
+			if !instr {
+				current=""
+			}
+		} else if instr {
+			out << h.strings
+		} else if current.trim_space() in conditionals {
 			for _ in 0..current.len-1 {
 				_:=out.pop()
 			}
 			for _ in current {
 				out << h.conditional
+			}
+			current=""
+		} else if current.trim_space() in types {
+			for _ in 0..current.len-1 {
+				_:=out.pop()
+			}
+			for _ in current {
+				out << h.types
+			}
+			current=""
+		} else if current.trim_space() in qualifiers {
+			for _ in 0..current.len-1 {
+				_:=out.pop()
+			}
+			for _ in current {
+				out << h.qualifiers
+			}
+			current=""
+		} else if current.trim_space() in other_keywords {
+			for _ in 0..current.len-1 {
+				_:=out.pop()
+			}
+			for _ in current {
+				out << h.other
+			}
+			current=""
+		} else if current.trim_space() in macros {
+			for _ in 0..current.len-1 {
+				_:=out.pop()
+			}
+			for _ in current {
+				out << h.macros
 			}
 			current=""
 		} else if c_ascii in ["{","}"] {
@@ -64,14 +184,16 @@ pub fn (h Highlighter) highlight(input string) []RGB {
 		} else if c_ascii in ["(",")","[","]"] {
 			out << h.brackets2
 			current=""
-		} else if c_ascii in ["'","\"","`"] {
-			instr=!instr
-			out << h.strings
-		} else if instr {
-			out << h.strings
-		}
-		else {
+		} else if c_ascii in ["0","1","2","3","4","5","6","7","8","9"] {
+			out << h.numbers
+		} else if c_ascii in symbols {
+			out << h.symbols
+		}else {
 			out << h.default
+		}
+
+		if c_ascii=="\n" {
+			current=""
 		}
 	}
 	return out
@@ -94,7 +216,7 @@ fn (mut l LiveC) function(defline string) {
 	mut inp:=""
 	for inp!="}" {
 		source+=inp+"\n"
-		inp=l.input("${" ".repeat(l.spaces*2)}")
+		inp=l.input("${" ".repeat(l.spaces*2)}", false)
 		l.check_and_out(inp)
 	}
 	l.functions << Function{
@@ -124,7 +246,7 @@ fn is_function(statement string) bool {
 }
 
 pub fn (mut l LiveC) source() string {
-	mut source:="${l.first}"
+	mut source:="${l.first}".trim(" ")
 	for func in l.functions {
 		source+=func.def
 		source+=func.source+"}"+"\n"
@@ -136,7 +258,7 @@ pub fn (mut l LiveC) source() string {
 }
 
 
-pub fn (l LiveC) input(prompt string) string {
+pub fn (l LiveC) input(prompt string, instring bool) string {
     mut read := Readline{}
     read.enable_raw_mode()
     mut out := ""
@@ -152,7 +274,7 @@ pub fn (l LiveC) input(prompt string) string {
         print('\033[F\033[K') // wipe current line
         print(temp)
 
-        l.hl.highlight(out).print(out)
+        l.hl.highlight(out, instring).print(out)
         println("")
 
         // Move the cursor to the correct position
@@ -190,28 +312,31 @@ fn (mut l LiveC) check_and_out(statement string) string {
 	]
 	first, _ := statement.split_once("(") or { "", "" }
 	mut temp_source:=""
-	if first.trim_space() in keywords {
+	if first.trim_space() in keywords || statement[statement.len-1].ascii_str()=="\\" {
+		temp_source+=" ".repeat((l.spaces*2)+2)+statement+"\n"
 		l.spaces+=2
-		temp_source+=statement+"\n"
 		for {
-			mut inp:=l.input("${" ".repeat(l.spaces*2)}")
+			mut inp:=l.input("${" ".repeat(l.spaces*2)}", statement[statement.len-1].ascii_str()=="\\")
 			if inp.contains_only("} ") {
-				temp:="${" ".repeat((l.spaces*2)-(1+first.len))}"
-				print('\033[F\033[K'.repeat(2))
+				l.spaces-=2
+				temp:="${" ".repeat(l.spaces*2)}"
+				print('\033[F\033[K')
 				print(temp)
-				l.hl.highlight('}').print('}')
+				l.hl.highlight('}', false).print('}')
 				println("")
-				
-				temp_source+="}\n"
+				temp_source+="${" ".repeat(l.spaces*2)}"+"}\n"
+				return temp_source
+			} else if inp[inp.len-1].ascii_str()==";" {
+				temp_source+=inp+"\n"
 				l.spaces-=2
 				return temp_source
 			} else {
 				temp_source+=l.check_and_out(inp)+"\n"
 			}
 		}
-		l.spaces-=2
+		
 	}
-	return statement
+	return " ".repeat((l.spaces*2)+2)+statement
 	
 }
 
@@ -222,7 +347,10 @@ pub fn (mut l LiveC) statement(statement string) {
 	if is_function(statement.trim_space()) {
 		l.function(statement)
 	} else if statement=="list" {
-		println(l.source())
+		print('\033[F\033[K')
+		source:=l.source()
+		l.hl.highlight(source, false).print(source)
+		println("")
 	} else if first=="#include" || first=="#define" {
 		l.first+=statement+"\n"
 	}
@@ -231,11 +359,13 @@ pub fn (mut l LiveC) statement(statement string) {
 		l.main_func+=l.check_and_out(statement)+"\n"
 		os.write_file("temp.c", l.source()) or { panic(err) }
 		output:=os.execute("tcc -w -run temp.c")
-		if output.exit_code==0 {
+		if output.output=="" {
+			l.last=""
+		} else if output.exit_code==0{
 			print('\033[F\033[K')
 			println("CC: ${output.output[l.last.len..]}")
 			l.last=output.output
-		} else {
+		} else{
 			l.main_func=main_func_bak
 			print('\033[F\033[K')
 			println("$?==${output.exit_code}: ${output.output}")
@@ -249,17 +379,30 @@ fn main() {
 		spaces: 0,
 		first: "
 #include <stdio.h>
-#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdbool.h>
+#include <errno.h>
+#include <assert.h>
+#include <malloc.h>
+#include <memory.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdint.h>
+#include <inttypes.h>
+#include <getopt.h>
 #include <math.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 ",
 		last: "",
 		hl: Highlighter{
             default: RGB{
-                r: 45,
-                g: 66,
-                b: 0
+                r: 200,
+                g: 200,
+                b: 200
             },
             conditional: RGB{
                 r: 204,
@@ -269,7 +412,7 @@ fn main() {
             numbers: RGB{
                 r: 61,
                 g: 114,
-                b: 0
+                b: 180
             },
             strings: RGB{
                 r: 99,
@@ -285,11 +428,37 @@ fn main() {
 				r: 230,
 				g: 180,
 				b: 30
+			},
+			types: RGB{
+				r: 0,
+				g: 245,
+				b: 120
+			},
+			qualifiers: RGB{
+				r: 0,
+				g: 50,
+				b: 255
+			},
+			other: RGB{
+				r: 0,
+				g: 155,
+				b: 255
+			},
+			macros: RGB{
+				r: 255,
+				g: 73,
+				b: 0
+			},
+			symbols: RGB{
+				r: 0,
+				g: 180,
+				b: 136
 			}
 
         }
 	}
+	println("")
 	for {
-		x.statement(x.input(""))
+		x.statement(x.input("", false))
 	}
 }
